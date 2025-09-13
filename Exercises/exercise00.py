9fe2932c6a7c343fef8d38f5a6ceb686946855a9
LucasZFriborg <Lucas.ZachauFriborg@iths.se>
2025-09-13 19:50:21 +0200

    first exercise done

--- Exercises/exercise00.py
+++ Exercises/exercise00.py
@@ -1,1 +1,15 @@
-print ("Hello1!")
+import math
+
+# a)
+
+a = 3
+b = 4
+c = math.sqrt(a**2 + b**2)
+print(f'The hypothenuse is {c} length units.')
+
+# b)
+
+c = 7.0
+a = 5.0
+b = math.sqrt(c**2 - a**2)
+print(f'The other cathetus is {round(b, 1)} length units.')
